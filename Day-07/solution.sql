b2cc9c8470e5902c9c4a714c0ca04e0a2316775e
Tamara Ndi <139602526+Missyl0@users.noreply.github.com>
2024-12-29 17:27:26 -0800

    Create solution.sql

---
+++ Day-07/solution.sql
@@ -0,0 +1,16 @@
+-- Solution for Day 7: SQL Advent Calendar
+
+SELECT 
+    v.vendor_name,
+    SUM(s.quantity_sold * s.price_per_unit) AS total_revenue
+FROM 
+    vendors v
+JOIN 
+    sales s
+ON 
+    v.vendor_id = s.vendor_id
+GROUP BY 
+    v.vendor_name
+ORDER BY 
+    total_revenue DESC
+LIMIT 2;
